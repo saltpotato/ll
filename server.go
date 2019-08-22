ac40b9b56f9c0162ea54c22dce196ddd59d25b92
saltpotato <martin2k@hotmail.de>
2019-08-22 13:23:45 +0200

    first state

---
+++ server.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Position struct {
+	From, Type, Org, OrgLink, Where, Does string
+	Topics                                []string
+	Href                                  []string
+}
+
+type Positions struct {
+	Poss []Position `json:"positions"`
+}
+
+func ServeIndexHTML(w http.ResponseWriter, req *http.Request) {
+
+	funcMap := template.FuncMap{
+		"Split": strings.Split,
+		"GetHREFText": func(position Position, token string) string {
+			if strings.HasPrefix(token, "\\") {
+
+				numPrefixed := token[1:]
+				numNTask := strings.Split(numPrefixed, ",")
+
+				if len(numNTask) < 2 {
+					return ""
+				}
+
+				idx, _ := strconv.Atoi(numNTask[0])
+				return position.Href[idx]
+			}
+			return ""
+		},
+		"StripHREFIndicator": func(token string) string {
+
+			idx := strings.Index(token, ",")
+			if idx != -1 && idx+1 < len(token) {
+				return token[idx+1:]
+			}
+			return ""
+		},
+	}
+
+	jsonFile, err := os.Open("positions.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Successfully Opened positions.json")
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// we initialize our Users array
+	var positions Positions
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'users' which we defined above
+	json.Unmarshal(byteValue, &positions)
+
+	tmpl, err := template.New("index_template.html").Funcs(funcMap).ParseFiles("pages/index_template.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "html")
+
+	err = tmpl.Execute(w, positions.Poss)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/bower_components/", func(w http.ResponseWriter, r *http.Request) {
+		path := "wwwroot/" + r.URL.Path[1:]
+		if strings.HasSuffix(path, ".css") {
+			http.ServeFile(w, r, path)
+		} else {
+
+			file, err := os.Open(path)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			http.ServeContent(w, r, "x", time.Time{}, file)
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		requestPath := r.URL.Path[1:]
+		if len(requestPath) == 0 {
+			ServeIndexHTML(w, r)
+		} else {
+			path := "/pages/" + requestPath
+			http.ServeFile(w, r, path)
+		}
+
+	})
+
+	err := http.ListenAndServeTLS(":443", "https-server.crt", "https-server.key", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
---
+++ wwwroot/startbash.cmd
@@ -0,0 +1,3 @@
+@echo on
+set PATH=%PATH%;"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Microsoft\VisualStudio\NodeJs\";"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Web\External\";"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Web\External\node_modules\.bin"
+"C:\Program Files\Git\bin\bash.exe"
